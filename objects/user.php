e4dabd7813da2b21a21e2e92ae000f6f02dbb363
Olya <omko.skp@edu.mercantec.dk>
2019-07-31 12:38:24 +0200

    verification of the user's email

--- objects/user.php
+++ objects/user.php
@@ -95,6 +95,7 @@ class User
 	                password = :password,
 	                udd_uid = :udd_uid,
 	                access_level = :access_level,
+	                access_code = :access_code,
 	                status = :status,
 	                created = :created";
 	 
@@ -110,6 +111,7 @@ class User
 	    $this->password=htmlspecialchars(strip_tags($this->password));
 	    $this->udd_uid=htmlspecialchars(strip_tags($this->udd_uid));
 	    $this->access_level=htmlspecialchars(strip_tags($this->access_level));
+	    $this->access_code=htmlspecialchars(strip_tags($this->access_code));
 	    $this->status=htmlspecialchars(strip_tags($this->status));
 	 
 	    // bind the values
@@ -127,6 +129,7 @@ class User
 	 	$stmt->bindParam(':udd_uid', $this->udd_uid);
 	 	
 	    $stmt->bindParam(':access_level', $this->access_level);
+	    $stmt->bindParam(':access_code', $this->access_code);
 	    $stmt->bindParam(':status', $this->status);
 	    $stmt->bindParam(':created', $this->created);
 	 
@@ -192,6 +195,63 @@ class User
 	 
 	    // return row count
 	    return $num;
+	}
+
+	//setting status to legal user efter email verification
+	function updateStatusByAccessCode(){
+	 
+	    // update query
+	    $query = "UPDATE " . $this->table_name . "
+	            SET status = :status
+	            WHERE access_code = :access_code";
+	 
+	    // prepare the query
+	    $stmt = $this->conn->prepare($query);
+	 
+	    // sanitize
+	    $this->status=htmlspecialchars(strip_tags($this->status));
+	    $this->access_code=htmlspecialchars(strip_tags($this->access_code));
+	 
+	    // bind the values from the form
+	    $stmt->bindParam(':status', $this->status);
+	    $stmt->bindParam(':access_code', $this->access_code);
+	 
+	    // execute the query
+	    if($stmt->execute()){
+	        return true;
+	    }
+	 
+	    return false;
+	}
+
+	// updates access code if user choosed "forgot password" option
+	function updateAccessCode(){
+	 
+	    // update query
+	    $query = "UPDATE
+	                " . $this->table_name . "
+	            SET
+	                access_code = :access_code
+	            WHERE
+	                email = :email";
+	 
+	    // prepare the query
+	    $stmt = $this->conn->prepare($query);
+	 
+	    // sanitize
+	    $this->access_code=htmlspecialchars(strip_tags($this->access_code));
+	    $this->email=htmlspecialchars(strip_tags($this->email));
+	 
+	    // bind the values from the form
+	    $stmt->bindParam(':access_code', $this->access_code);
+	    $stmt->bindParam(':email', $this->email);
+	 
+	    // execute the query
+	    if($stmt->execute()){
+	        return true;
+	    }
+	 
+	    return false;
 	}
 
 }
